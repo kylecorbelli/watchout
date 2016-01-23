654133f5e0243ee6e0dd725cf69826476a4c6dd0
pacificLiving <kyle.corbelli@gmail.com>
2016-01-22 16:15:07 -0800

    Make it so that the enemies move to a new random location every second

--- client/watchout.js
+++ client/watchout.js
@@ -5,23 +5,27 @@ var asteroidCount = 10;
 var asteroids = [];
 
 var w = window;
-var width = w.width;
-var height = w.height;
+var width = w.innerWidth;
+var height = w.innerHeight;
+
+var ramboX = function(){
+  return Math.max(Math.floor(Math.random() * width) - 200, 200);
+};
+
+var ramboY = function(){
+  return Math.min(Math.floor(Math.random() * height), height - 200);
+};
 
 for(var i=0; i<asteroidCount; i++){
   var dataObject = {
-    x : Math.floor(Math.random() * window.innerWidth),
-    y : Math.min(Math.floor(Math.random() * window.innerHeight), 900)
+    x : ramboX(),
+    y : ramboY()
   };
   asteroids.push(dataObject);
 }
 
 
-
-
 d3.select('.board').style('width', width).style('height', height);
-
-
 
 var select = d3.select('svg').selectAll('.asteroid');
 
@@ -35,3 +39,24 @@ select.data(asteroids)
     return d.y;
   })
   .attr('xlink:href', asteroidImage);
+
+
+var moveRocks = function() {
+
+  var determineNextXLoc = function(){
+    return ramboX();
+  }
+  var determineNextYLoc = function(){
+    return ramboY();
+  }
+
+
+  var rocks = d3.select('svg').selectAll('image');
+  rocks.transition()
+  .attr("x", determineNextXLoc)
+  .attr("y", determineNextYLoc)
+  .duration(500);
+
+};
+
+setInterval(moveRocks, 1000);
